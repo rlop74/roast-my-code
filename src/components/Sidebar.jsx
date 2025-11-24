cc9fbfa0d3234061e462f00a067b2ea52063c7ef
rlop74 <119091421+rlop74@users.noreply.github.com>
2025-11-23 22:57:43 -0500

    - worked on Sidebar component - setup Link for the menu items - fixed Sidebar layout - did not work on theme toggle for now

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,51 @@
-import { LayoutDashboard, Users, ChessQueen, TrophyIcon, CircleUserRound } from "lucide-react";
+import { Link } from "react-router";
+
+import {
+    LayoutDashboard,
+    Users,
+    ChessQueen,
+    TrophyIcon,
+    CircleUserRound,
+    Settings,
+} from "lucide-react";
 
 export const Sidebar = () => {
     const upperMenuItems = [
-        { name: "Home", icon: <LayoutDashboard /> },
-        { name: "Following", icon: <Users /> },
-        { name: "Roast Arena", icon: <ChessQueen /> },
-        { name: "Leaderboard", icon: <TrophyIcon /> },
+        { name: "Home", icon: <LayoutDashboard />, path: "/" },
+        { name: "Following", icon: <Users />, path: "/following" },
+        { name: "Roast Arena", icon: <ChessQueen />, path: "/roast-arena" },
+        { name: "Leaderboard", icon: <TrophyIcon />, path: "/leaderboard" },
     ];
     const lowerMenuItems = [
-        {name: "Profile", icon: <CircleUserRound />}, 
-        {name: "Theme Toggle", icon: ""},
+        { name: "Profile", icon: <CircleUserRound />, path: "/profile" },
+        { name: "Settings", icon: <Settings />, path: "/settings" },
+        { name: "Theme Toggle", icon: "", path: "" },
     ];
 
     return (
         <div className="flex flex-col justify-between w-full p-8 list-none">
-            <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-4">
                 {upperMenuItems.map((item) => (
-                    <li key={item.name} className="flex gap-2 cursor-pointer hover:bg-[#F4F4F8]/50 p-5 rounded-full">
-                        {item.icon} {item.name}
-                    </li>
+                    <Link to={item.path}>
+                        <li
+                            key={item.name}
+                            className="flex gap-2 cursor-pointer hover:bg-[#F4F4F8]/50 p-5 rounded-full"
+                        >
+                            {item.icon} {item.name}
+                        </li>
+                    </Link>
                 ))}
             </div>
-            <div className="flex flex-col gap-20">
+            <div className="flex flex-col gap-2">
                 {lowerMenuItems.map((item) => (
-                    <li key={item.name} className="flex gap-2 cursor-pointer hover:bg-[#F4F4F8]/50 p-5 rounded-full">{item.icon} {item.name}</li>
+                    <Link to={item.path}>
+                        <li
+                            key={item.name}
+                            className="flex gap-2 cursor-pointer hover:bg-[#F4F4F8]/50 p-5 rounded-full"
+                        >
+                            {item.icon} {item.name}
+                        </li>
+                    </Link>
                 ))}
             </div>
         </div>
